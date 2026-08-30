74a888e961e22e4b0e6b32862447fa7bf6acd3f9
Mishie Mangarateke <mangamthe1@gmail.com>
2026-08-30 20:54:02 +0200

    feat: connect production lead endpoint

--- site-config.js
+++ site-config.js
@@ -1,7 +1,6 @@
 window.BAHOLO_LEAD_CONFIG = {
-  // Paste the deployed Google Apps Script Web App URL here after setup.
-  // Example shape: https://script.google.com/macros/s/AKfycb.../exec
-  LEAD_ENDPOINT_URL: '',
+  // Public Apps Script Web App receiver. No Google credentials are embedded.
+  LEAD_ENDPOINT_URL: 'https://script.google.com/macros/s/AKfycbwVbe9Whe9MLfh0C4RSRLk3fv0n6EaGuLP6D275FDSqdfs1TMVCgQq9PydvLbBWqz-K/exec',
   FORM_VERSION: '2026-08-30',
   PRIVACY_VERSION: '2026-08-30',
   GA_MEASUREMENT_ID: 'G-MKR7FTEKE8'
